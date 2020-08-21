1161daed446fc273726f029ff949753d54b8e5e7
Anjali Sharma <anjalishar89@gmail.com>
2020-08-21 11:07:33 -0700

    Adds initial components

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Nav from "../../Components/Nav";
+import Header from "../../Components/Header";
+import Work from "../../Components/Work";
+import Contact from "../../Components/Contact";
+
+const Home = () => {
+
+
+return (
+
+  <>
+    <Nav />
+    <Header />
+    <Work />
+    <Contact />
+  </>
+
+
+
+);
+};
+
+export default Home;
