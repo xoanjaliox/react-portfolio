dab4f1710161ffb2210b4b68fe123ebc358e2eb1
Anjali Sharma <anjalishar89@gmail.com>
2020-09-12 11:16:52 -0700

    updates work section

--- client/src/Components/Work/index.js
+++ client/src/Components/Work/index.js
@@ -7,11 +7,24 @@ const Work = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            
+            <div className="card">
+              <img className="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+              <div className="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">
+                  Some quick example text to build on the card title
+                  and make up the bulk of the card's content.
+                </p>
+    <a href="#!" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
           </div>
           <div className="col-md-6">
             <h3>Hi! My name is Anjali</h3>
-            <p>Mario ipsum RGB mushroom 1-up. Cheep cheep lava bill blaster podoboo goomba Nintendo Entertainment System bullet bill game pak hammer bro game over radio frequency modulator life, bridge bullet bill brick select button. Life Mario Bros cloud invincible Luigi.</p>
+            <p>
+              Mario ipsum RGB mushroom 1-up. Cheep cheep lava bill blaster podoboo goomba Nintendo Entertainment System bullet bill game pak hammer bro game over radio frequency modulator life, bridge bullet bill brick select button. Life Mario Bros cloud invincible Luigi.
+            </p>
+  
           </div>   
         </div>
       </div>
