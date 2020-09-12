c881345d78351bcc3d57ba2c2af23d67d6d0bc42
Anjali Sharma <anjalishar89@gmail.com>
2020-09-12 11:51:34 -0700

    updates header and work sections

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -8,7 +8,9 @@ const Header = () => {
         <div className="row">
           <div className="col-md-6">
             <h3>Hi! My name is Anjali</h3>
-            <p>Mario ipsum RGB mushroom 1-up. Cheep cheep lava bill blaster podoboo goomba Nintendo Entertainment System bullet bill game pak hammer bro game over radio frequency modulator life, bridge bullet bill brick select button. Life Mario Bros cloud invincible Luigi.</p>
+            <p>
+              Mario ipsum RGB mushroom 1-up. Cheep cheep lava bill blaster podoboo goomba Nintendo Entertainment System bullet bill game pak hammer bro game over radio frequency modulator life, bridge bullet bill brick select button. Life Mario Bros cloud invincible Luigi.
+            </p>
           </div>
           <div className="col-md-6">
             <img src="https://www.fillmurray.com/1000/1000" className="img-fluid" alt="Responsive image" /> 
