dc33b0be6a78107ba633ae13efb3fce3d8463c88
Anjali Sharma <anjalishar89@gmail.com>
2020-09-29 16:01:21 -0700

    Updates skills section

--- client/src/Components/Skills/index.js
+++ client/src/Components/Skills/index.js
@@ -7,15 +7,20 @@ const Skills = () => {
       <>
       <div className="skills-area">
         <div className="container white-box">
-          <h3>Skills</h3>
+          <div className="col-md-12 section-title">Skills</div>
+
+          <div className="row des-dev">
+            <div className="col-md-6">
+              <h4>Designer</h4>
+            </div>
+            <div className="col-md-6">
+              <h4>Developer</h4>
+            </div>
+          </div>
+
+
         </div>
       </div>
-
-
-
-      
-      
-
     </>
     
     );
