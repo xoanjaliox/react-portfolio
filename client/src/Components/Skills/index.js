0ec24364aa84b1acd9e524e79c7e90937bef22b4
Anjali Sharma <anjalishar89@gmail.com>
2020-10-24 10:56:42 -0700

    adds updated files

--- client/src/Components/Skills/index.js
+++ client/src/Components/Skills/index.js
@@ -10,11 +10,31 @@ const Skills = () => {
           <div className="col-md-12 section-title">Skills</div>
 
           <div className="row des-dev">
-            <div className="col-md-6">
+            <div className="col-md-6 design">
               <h4>Designer</h4>
+              <div className="row iconsr1">
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+              </div>
+              <div className="row iconsr2">
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+              </div>
+              
             </div>
-            <div className="col-md-6">
+            <div className="col-md-6 dev">
               <h4>Developer</h4>
+              <div className="row iconsr1">
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+              </div>
+              <div className="row iconsr2">
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+                <img className="img-responsive" src={"assets/images/photoshopicon.png"} />
+              </div>
             </div>
           </div>
 
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 const root = require('path').join(__dirname, 'client', 'build')
 app.use(express.static(root));
 
+
 app.get("*", (req, res) => {
   res.sendFile('index.html', {
     root
